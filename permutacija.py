06a105b8856f8289397065f9a9093d6e20cb1fd3
Luka <luka.lajovic@hotmail.com>
2016-12-25 00:38:01 +0100

    veni vedi vici

--- permutacija.py
+++ permutacija.py
@@ -14,6 +14,7 @@ class Permutacija:
             return j
         else:
             return "sranje"
+    
     def __repr__(self):
         return str(self.seznam)
     def __len__(self):
@@ -22,8 +23,15 @@ class Permutacija:
     def __mul__(self,druga):
             k=self.permutiraj(druga.seznam)
             return Permutacija(k,self.n)
-                
+    def inverz(self):
+        j=[0 for u in range(self.n)]
+        for x in range(self.n):
+            j[self.seznam[x]]=x
+            
+        return Permutacija(j,self.n)
     
 
 p=Permutacija([0,2,1],3)
+p2=Permutacija([3,0,1,2])
+print(p2.inverz())
 print(p.permutiraj(["a","b","c"]))
